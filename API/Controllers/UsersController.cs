be363ac4c9dee51a5a87db3b36fb335867cbbe28
William Schweitzer <weschweitzer@gmail.com>
2025-03-10 11:19:49 -0400

    Add Angular, bootstrap, CORS, first component

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -13,10 +13,9 @@ public class UsersController(DataContext context) : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await context.Users.ToListAsync ();
+        var users = await context.Users.ToListAsync();
 
         return users;
-        // git test
     }
 
     [HttpGet("{id}")]
--- API/Program.cs
+++ API/Program.cs
@@ -4,18 +4,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
